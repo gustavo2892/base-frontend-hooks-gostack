61616790a0077cc7ea38f5301cbc85e3ea925e06
gustavo2892 <gustavo2892@gmail.com>
2020-03-09 23:14:12 -0300

    First Commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <Title>
+        Edit <code>pages/Main/index.js</code> and save to reload.
+      </Title>
+    </Container>
+  );
+}
+
+export default Main;
