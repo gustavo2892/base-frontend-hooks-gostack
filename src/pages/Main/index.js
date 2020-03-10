ba06cc8cad604fdade5795575c8192ba2a7523a7
gustavo2892 <gustavo2892@gmail.com>
2020-03-10 14:36:27 -0300

    Basic base finalized

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
-import { Container, Title } from './styles';
+import { Container } from './styles';
 
 function Main() {
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  // ComponentDidMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // ComponentWillUpdate
+    // return () => {};
+  }, []);
+
+  // ComponentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <Container>
-      <Title>
-        Edit <code>pages/Main/index.js</code> and save to reload.
-      </Title>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <br />
+      <strong>Você tem {techSize} tecnologias.</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
     </Container>
   );
 }
